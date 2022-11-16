4b5eabd73f11f5f8616d3b358b752102276bdfd0
JongGeun <dldvk9999@gmail.com>
2022-11-17 07:54:25 +0900

    heroku db connect

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,19 @@
 import mysql from "mysql";
 
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "whdrms6533@",
+//     port: 3306,
+//     database: "portfolio-backend",
+// });
+
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "whdrms6533@",
+    host: "qvti2nukhfiig51b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "bat2jf9b6hiqo3jp",
+    password: "uuj1u94v8yjys00k",
     port: 3306,
-    database: "portfolio-backend",
+    database: "vgtusfzjs3r27hhl",
 });
 
 export default connection;
