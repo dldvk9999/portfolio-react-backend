ec136a2c77ee1fa653e13d9ba52c6a38780a2ad4
JongGeun <dldvk9999@naver.com>
2022-12-07 08:40:47 +0900

    config rollback

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,23 +1,11 @@
 import mysql from "mysql";
 
-// const connection = mysql.createConnection({
-//     host: "qvti2nukhfiig51b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-//     user: "bat2jf9b6hiqo3jp",
-//     password: "uuj1u94v8yjys00k",
-//     port: 3306,
-//     database: "vgtusfzjs3r27hhl",
-// });
-
 const connection = mysql.createConnection({
-    host: "gateway01.us-west-2.prod.aws.tidbcloud.com",
-    user: "47bM2TMyXaSgn5X.root",
-    password: "Whdrms6533@",
-    port: 4000,
-    database: "test",
-    ssl: {
-        minVersion: "TLSv1.2",
-        rejectUnauthorized: false,
-    },
+    host: "qvti2nukhfiig51b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "bat2jf9b6hiqo3jp",
+    password: "uuj1u94v8yjys00k",
+    port: 3306,
+    database: "vgtusfzjs3r27hhl",
 });
 
 export default connection;
