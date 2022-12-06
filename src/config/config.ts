b07aec0d5c61ce6d61a8d9bd851f4aeecc604dbc
JongGeun <dldvk9999@naver.com>
2022-12-06 10:38:10 +0900

    env patch

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,14 +1,6 @@
 import mysql from "mysql";
 
 // const connection = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "whdrms6533@",
-//     port: 3306,
-//     database: "portfolio-backend",
-// });
-
-// const connection = mysql.createConnection({
 //     host: "qvti2nukhfiig51b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
 //     user: "bat2jf9b6hiqo3jp",
 //     password: "uuj1u94v8yjys00k",
@@ -16,11 +8,23 @@ import mysql from "mysql";
 //     database: "vgtusfzjs3r27hhl",
 // });
 
+// const connection = mysql.createConnection({
+//     host: "gateway01.us-west-2.prod.aws.tidbcloud.com",
+//     user: "47bM2TMyXaSgn5X.root",
+//     password: "",
+//     port: 4000,
+//     database: "test",
+//     ssl: {
+//         minVersion: "TLSv1.2",
+//         rejectUnauthorized: true,
+//     },
+// });
+
 const connection = mysql.createConnection({
-    host: "gateway01.us-west-2.prod.aws.tidbcloud.com",
-    user: "47bM2TMyXaSgn5X.root",
-    password: "Whdrms6533@",
-    port: 4000,
+    host: process.env.TIDB_HOST,
+    user: process.env.TIDB_USER,
+    password: process.env.TIDB_PASSWORD,
+    port: Number(process.env.TIDB_PORT),
     database: "test",
     ssl: {
         minVersion: "TLSv1.2",
