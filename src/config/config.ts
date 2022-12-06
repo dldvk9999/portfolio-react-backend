691bb168bdfef125b50a5c3692a3108730682f2b
JongGeun <dldvk9999@naver.com>
2022-12-06 10:30:13 +0900

    db url change

--- src/config/config.ts
+++ src/config/config.ts
@@ -8,12 +8,24 @@ import mysql from "mysql";
 //     database: "portfolio-backend",
 // });
 
+// const connection = mysql.createConnection({
+//     host: "qvti2nukhfiig51b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//     user: "bat2jf9b6hiqo3jp",
+//     password: "uuj1u94v8yjys00k",
+//     port: 3306,
+//     database: "vgtusfzjs3r27hhl",
+// });
+
 const connection = mysql.createConnection({
-    host: "qvti2nukhfiig51b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "bat2jf9b6hiqo3jp",
-    password: "uuj1u94v8yjys00k",
-    port: 3306,
-    database: "vgtusfzjs3r27hhl",
+    host: "gateway01.us-west-2.prod.aws.tidbcloud.com",
+    user: "47bM2TMyXaSgn5X.root",
+    password: "Whdrms6533@",
+    port: 4000,
+    database: "test",
+    ssl: {
+        minVersion: "TLSv1.2",
+        rejectUnauthorized: true,
+    },
 });
 
 export default connection;
